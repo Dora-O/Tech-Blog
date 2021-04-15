8f41d1ec024b4dffa1f7f76f0c37e018881fb3d2
Dora O <dorozco2390@utexas.edu>
2021-04-14 22:03:11 -0500

    fixed issue preventing user input from being stored in database

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,26 +1,30 @@
+const button = document.querySelector('#button');
 const signupFormHandler = async (event) => {
-    event.preventDefault();
-  
-    // Collect sign up form values
-    const username = document.querySelector('#').value.trim();
-    const password = document.querySelector('#').value.trim();
-  
-    if (username && password) {
-      const response = await fetch('ENTER PATH', {
-        method: 'POST',
-        body: JSON.stringify({ username, password }),
-        headers: { 'Content-Type': 'application/json' },
-      });
-  
-      if (response.ok) {
-        document.location.replace('/');
-      } else {
-        alert("Please make sure your password is at least 8 characters long");
-      }
+  // event.preventDefault();
+
+  // Collect sign up form values
+  const username = document.querySelector('#username-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
+
+  if (username && password) {
+    const response = await fetch('/api/users', {
+      method: 'POST',
+      body: JSON.stringify({ username, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      document.location.replace('/');
+      console.log('success')
+    } else {
+      alert("Please make sure your password is at least 8 characters long");
     }
-  };
-  
-  // Event Listener
-  document
-    .querySelector('#')
-    .addEventListener('submit', signupFormHandler);
+  }
+};
+
+// Event Listener
+document
+  .querySelector('.signup-form')
+  .addEventListener('submit', signupFormHandler);
+
+button.onclick = signupFormHandler();
